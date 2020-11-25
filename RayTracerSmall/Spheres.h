d76ea8ebef17adb603c84686c239c879fc326d09
magiclollyl <37641946+magiclollyl@users.noreply.github.com>
2020-11-25 22:46:49 +0000

    Initial commit

---
+++ RayTracerSmall/Spheres.h
@@ -0,0 +1,52 @@
+#pragma once
+#include "Vector3.h"
+#include <stdlib.h>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <cassert>
+// Windows only
+#include <algorithm>
+#include <sstream>
+#include <string.h>
+
+#if defined __linux__ || defined __APPLE__
+// "Compiled for Linux
+#else
+// Windows doesn't define these values by default, Linux does
+#define M_PI 3.141592653589793
+#define INFINITY 1e8
+#endif
+
+class Sphere
+{
+typedef Vec3<float> Vec3f;
+public:
+	Vec3f center;                           /// position of the sphere
+	float radius, radius2;                  /// sphere radius and radius^2
+	Vec3f surfaceColor, emissionColor;      /// surface color and emission (light)
+	float transparency, reflection;         /// surface transparency and reflectivity
+
+	Sphere(
+		const Vec3f& c,
+		const float& r,
+		const Vec3f& sc,
+		const float& refl = 0,
+		const float& transp = 0,
+		const Vec3f& ec = 0) :
+		center(c), radius(r), radius2(r* r), surfaceColor(sc), emissionColor(ec),
+		transparency(transp), reflection(refl)
+	{ /* empty */
+	}
+
+	float mix(const float& a, const float& b, const float& mix);
+	bool intersect(const Vec3f& rayorig, const Vec3f& raydir, float& t0, float& t1) const;
+	Vec3f trace(const Vec3f& rayorig, const Vec3f& raydir, const std::vector<Sphere>& spheres, const int& depth);
+	void render(const std::vector<Sphere>& spheres, int iteration);
+	void BasicRender();
+	void SimpleShrinking();
+	void SmoothScaling();
+};
+
---
+++ RayTracerSmall/main.cpp
@@ -0,0 +1,60 @@
+// [header]
+// A very basic raytracer example.
+// [/header]
+// [compile]
+// c++ -o raytracer -O3 -Wall raytracer.cpp
+// [/compile]
+// [ignore]
+// Copyright (C) 2012  www.scratchapixel.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// [/ignore]
+#include <stdlib.h>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <cassert>
+// Windows only
+#include <algorithm>
+#include <sstream>
+#include <string.h>
+#include "Spheres.h"
+
+#if defined __linux__ || defined __APPLE__
+// "Compiled for Linux
+#else
+// Windows doesn't define these values by default, Linux does
+#define M_PI 3.141592653589793
+#define INFINITY 1e8
+#endif
+
+//[comment]
+// In the main function, we will create the scene which is composed of 5 spheres
+// and 1 light (which is also a sphere). Then, once the scene description is complete
+// we render that scene, by calling the render() function.
+//[/comment]
+int main(int argc, char **argv)
+{
+	Sphere* spheres = new Sphere();
+	// This sample only allows one choice per program execution. Feel free to improve upon this
+	srand(13);
+	//BasicRender();
+	//SimpleShrinking();
+	spheres.SmoothScaling();
+
+	return 0;
+}
+
